0334868c5ac804632d4d62d3c2d3a8a6c4869a54
AdrienW86 <61838504+AdrienW86@users.noreply.github.com>
2022-02-10 13:41:24 +0100

    Update functions.js

--- Js/functions.js
+++ Js/functions.js
@@ -1,76 +1,86 @@
-import {uniqueIngredient, uniqueAppareil, uniqueUstensil} from "./datas.js" 
+import Recipe from "./class-recipes.js"
+import Listbox from "./class-listbox.js"
 
-  // Création des listes
-  export function buildArray() {
-        uniqueIngredient.forEach(ingredient => { 
-            let list = document.querySelector(".list-li0")
-            let li = document.createElement("li")
-                li.setAttribute("class","option")
-                li.innerHTML = ` <button class= "btn-one-choice" value="ingredient"> ${ingredient} </button>`
-                list.appendChild(li)
-        })
-        uniqueAppareil.forEach(appareil => { 
-            let list = document.querySelector(".list-li1")
-            let li = document.createElement("li")
-                li.setAttribute("class","option")
-                li.innerHTML = ` <button class= "btn-one-choice" value="appareil"> ${appareil} </button>`
-                list.appendChild(li)
-        })
-        uniqueUstensil.forEach(ustensil => { 
-            let list = document.querySelector(".list-li2")
-            let li = document.createElement("li")
-                li.setAttribute("class","option")
-                li.innerHTML = ` <button class= "btn-one-choice" value="ustensil"> ${ustensil} </button>`
-                list.appendChild(li)
-        })
+// Ouverture de la listbox
+export function openArray(e) {  
+  let listboxs = document.querySelectorAll(".listbox")
+  let listboxopens = document.querySelectorAll(".listboxopen")
+      listboxs.forEach(listbox => listbox.style.display = "flex")
+      listboxopens.forEach(listboxopen => listboxopen.style.display = "none")
+        e.target.closest(".listbox").style.display = "none"
+        e.target.closest(".listbox").nextSibling.style.display = "flex"
+}
+// Fermeture de la listbox  
+export function closeArray(e) {
+  let list = document.querySelectorAll(".listbox")
+  let array = document.querySelectorAll(".listboxopen")
+  array.forEach(tab => tab.style.display = "none")
+  list.forEach(listbox => listbox.style.display = "flex")
+}
+// Afficher toutes les recettes
+export function getAllRecipe(array) {
+  array.forEach(recette => {
+    const listOfRecipes = new Recipe(recette)
+          listOfRecipes.buildRecipe()          
+  })
+}
+// Création des listbox filtrées
+export function creationDesListbox(array, arrayTag) {
+  let arrayIngredients = []; let arrayAppareils = []; let arrayUstensils = []
+  array.forEach(recette => {
+    arrayAppareils.push(recette.appliance.charAt(0).toUpperCase() + recette.appliance.substring(1).toLowerCase())
+      recette.ingredients.forEach(ingredient => {
+        arrayIngredients.push(ingredient.ingredient.charAt(0).toUpperCase() + ingredient.ingredient.substring(1).toLowerCase())        
+      })
+      recette.ustensils.forEach(ustensil => {
+        arrayUstensils.push(ustensil.charAt(0).toUpperCase() + ustensil.substring(1).toLowerCase())
+      })
+  })
+  arrayIngredients = [...new Set(arrayIngredients.sort((a,b) => a.localeCompare(b, 'fr',{sensitivity: 'base'})))]
+  arrayAppareils = [...new Set(arrayAppareils.sort((a,b) => a.localeCompare(b, 'fr',{sensitivity: 'base'})))]
+  arrayUstensils = [...new Set(arrayUstensils.sort((a,b) => a.localeCompare(b, 'fr',{sensitivity: 'base'})))]
+  array = [...new Set(array.sort())]
+
+  console.table(arrayUstensils)
+  // On supprime l'élement tagué dans la liste
+  if(arrayTag) {
+   arrayTag.forEach(tag => {
+      if(arrayIngredients.includes(tag)) {
+        arrayIngredients.splice(arrayIngredients.indexOf(tag), 1)
+      }
+      else if(arrayAppareils.includes(tag)) {
+        arrayAppareils.splice(arrayAppareils.indexOf(tag), 1)
+      }else if(arrayUstensils.includes(tag)) {
+        arrayUstensils.splice(arrayUstensils.indexOf(tag), 1)
+      }
+   })   
   }
-  // Ouverture de la listbox
-  export function openArray(e) {        
-    let target = e.target 
-      if(target.getAttribute("class") == "select-btn0") {                 
-        document.querySelector(".array0").style.display ="flex"
-        document.querySelector(".listbox0").style.display = "none"
-      }else if(target.getAttribute("class") == "select-btn1") {                             
-        document.querySelector(".array1").style.display ="flex"
-        document.querySelector(".listbox1").style.display = "none"
-      }else if(target.getAttribute("class") == "select-btn2") {                             
-        document.querySelector(".array2").style.display ="flex"
-        document.querySelector(".listbox2").style.display = "none"
-      }  
-  }
-  // Fermeture de la listbox  
-  export function closeArray(e) {
-    e.target.closest(".active").style.display ="none"
-    e.target.closest(".active").previousSibling.style.display ="flex"
-  }
-  // Création d'un tag
-  export function createTag(e) {
-    let tagContainer = document.querySelector(".tag-container")
-        tagContainer.style.display = "flex"
-       let div = document.createElement("div")
-           div.innerHTML = `<p> ${e.target.innerHTML} </p> <button class="btn-close-tag" </button>`
-           tagContainer.appendChild(div)
-          if(e.target.value == "ingredient") {
-            div.setAttribute("class","tag-ingredient")
-          }
-          else if(e.target.value == "appareil") {
-            div.setAttribute("class","tag-appareil")
-          }
-          else if(e.target.value == "ustensil") {
-            div.setAttribute("class","tag-ustensil")
-          }
-          closeArray(e)
-          document.querySelectorAll(".btn-close-tag").forEach(btn => btn.addEventListener("click", deleteTag))
-  }
-  // Suppression d'un tag 
-  export function deleteTag(e) {
-    console.log(e.target.closest(".active"))
-    let tag = e.target.parentElement
-    console.log(tag)
-    tag.remove()
-  }
-  export function filterArray(uniqueIngredient, uniqueAppareil, uniqueUstensil) {
-    if(document.querySelector(".tag-ingredient")) {
-      console.log(uniqueIngredient)
-    }
-  } 
+  let list = document.querySelector(".list-li0")
+  let list1 = document.querySelector(".list-li1")
+  let list2 = document.querySelector(".list-li2")
+
+  let search = document.querySelector(".find0")
+  let search1 = document.querySelector(".find1")
+  let search2 = document.querySelector(".find2")
+  
+  afficherMessage(arrayIngredients, list, search)
+  afficherMessage(arrayAppareils, list1, search1)
+  afficherMessage(arrayUstensils, list2, search2)
+
+  const listIngredients = new Listbox(arrayIngredients, arrayAppareils, arrayUstensils )
+  listIngredients.buildArray() 
+}
+
+function afficherMessage (array, target, search) {
+  let message = document.createElement("div")
+      message.setAttribute("class", "message")
+      message.innerHTML = "Aucun élément à afficher !"
+      target.appendChild(message)
+      if(array.length == 0) {
+        message.style.display = "block"
+        search.style.display = "none"
+      }else{
+        message.style.display = "none"
+        search.style.display = "block"
+      }
+}
