143e4f49d0b25bb5147b2c8accf09b9e69448e71
AdrienW86 <61838504+AdrienW86@users.noreply.github.com>
2022-02-10 13:43:16 +0100

    Create listbox.js

---
+++ Js/listbox.js
@@ -0,0 +1,36 @@
+export function buildListbox() {
+  for(let i = 0; i < 3; i++) {
+      let classname = "listbox" 
+      let title = "title" + i
+      let find = "find" + i 
+      let selectBtn = "select-btn" + i
+      let close = "btn-close" 
+      let ulbox = "ul-box" + i
+      let list = "list-li" + i
+      let section = document.querySelector(".listbox-container")
+      let listbox = document.createElement("div")        
+          listbox.classList.add(classname)
+          listbox.classList.add (classname + i)
+          section.appendChild(listbox)
+          listbox.innerHTML = `<div class="listbox-header">
+                                    <div class="${title}"></div>                                
+                                      <button class="${selectBtn}"></button>
+                              </div> `                              
+      let listbox_open = document.createElement("div")
+          listbox_open.classList.add(classname + "open") 
+          section.appendChild(listbox_open)
+          listbox_open.innerHTML = `<div class="array">  
+                                      <div class=" ${ulbox}">
+                                        <input class="find ${find}" type="search"/>
+                                        <button class="${close}"></button>                         
+                                        <ul class="ul ${list}"></ul>                                    
+                                      </div>
+                                    </div>`                     
+  }
+  document.querySelector(".title0").innerHTML = "Ingrédients"
+  document.querySelector(".find0").setAttribute("placeholder", "Rechercher un ingrédient")
+  document.querySelector(".title1").innerHTML = "Appareils"
+  document.querySelector(".find1").setAttribute("placeholder", "Rechercher un appareil")
+  document.querySelector(".title2").innerHTML = "Ustensiles"
+  document.querySelector(".find2").setAttribute("placeholder", "Rechercher un ustensile")   
+}
