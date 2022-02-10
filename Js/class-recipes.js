79917893524832a28e517b231d5db10580a91b85
AdrienW86 <61838504+AdrienW86@users.noreply.github.com>
2022-02-10 13:39:41 +0100

    Update class-recipes.js

--- Js/class-recipes.js
+++ Js/class-recipes.js
@@ -11,9 +11,8 @@ export default class Recipe {
         this.ustensils = data.ustensils
     }
 
-    buildRecipe() {
-
-        const main = document.getElementById("container")
+    buildRecipe() {   
+        const container = document.querySelector("#container")
         const card = document.createElement("section")
         const headercard = document.createElement("div")
         const picture = document.createElement("img")
@@ -22,12 +21,10 @@ export default class Recipe {
         const cardContent = document.createElement("section")
         const divUl = document.createElement("div")
         const divDescription = document.createElement("div")
-        
-        
         const ul = document.createElement("ul")
         const description = document.createElement("div")
         
-        main.appendChild(card)
+        container.appendChild(card)
         card.appendChild(headercard) 
         headercard.appendChild(picture)      
         card.appendChild(bodyCard)
@@ -40,13 +37,14 @@ export default class Recipe {
        
         card.setAttribute("class","fiche_recette")
         card.setAttribute("data-appliance", this.appliance)
-        card.setAttribute("data-ustensils", this.ustensils)
+        card.setAttribute("data-ustensils", this.ustensils )
         headercard.setAttribute("class", "header_recette")
         picture.setAttribute("class", "picture")
         picture.setAttribute("alt", "photo de l'image")
         bodyCard.setAttribute("class", "body_recette")
         description.setAttribute("class", "bloc-recette" )
         title.setAttribute("class", "box-title")
+        cardContent.setAttribute("class", "article")
         divUl.setAttribute("class", "ul-container")
         divDescription.setAttribute("class", "description-container")
         ul.setAttribute("class", "liste_ingredients")
@@ -57,22 +55,27 @@ export default class Recipe {
                                 <svg width="14" height="14" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM10 18C5.59 18 2 14.41 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 14.41 14.41 18 10 18ZM10.5 5H9V11L14.2 14.2L15 12.9L10.5 10.2V5Z" fill="black"/>
                                 </svg> ${this.time} min 
-                            </p>`
-        
-        const items = this.ingredients
-        items.forEach(item => {
-            const li = document.createElement("li")
-            ul.appendChild(li)
-            if(item.quantity && item.unit) {
-                li.innerHTML = item.ingredient + ": " + item.quantity + " " + item.unit                      
-            }else if(item.quantity && ! item.unit){
-                li.innerHTML = item.ingredient + ": " + item.quantity 
-            }else if(!item.quantity && item.unit) {
-                li.innerHTML = item.ingredient + item.unit 
-            }else{
-                li.innerHTML = item.ingredient 
-            }
-        }) 
-        description.innerHTML = this.description     
+                            </p>`                            
+        const items = this.ingredients      
+        let itemArray = []
+            items.forEach(item => {
+                const li = document.createElement("li")           
+                for(let i = 0 ; i < items.length ; i++) {           
+                    itemArray.push(items[i].ingredient)             
+                }
+                ul.appendChild(li)
+                if(item.quantity && item.unit) {
+                    li.innerHTML = `<span class= "span_ingredient"> ${item.ingredient} </span> : ${item.quantity} ${item.unit}`                    
+                }else if(item.quantity && ! item.unit){
+                    li.innerHTML = `<span class= "span_ingredient"> ${item.ingredient} </span> : ${item.quantity}`
+                }else if(!item.quantity && item.unit) {
+                    li.innerHTML = `<span class= "span_ingredient"> ${item.ingredient} </span> : ${item.unit}` 
+                }else{
+                    li.innerHTML = `<span class= "span_ingredient"> ${item.ingredient} </span>` 
+                }         
+            }) 
+        let newArray= [... new Set(itemArray)]
+        card.setAttribute("data-ingredient", newArray)
+        description.innerHTML = this.description    
     }
 }
